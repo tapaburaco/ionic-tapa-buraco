5c5c871d2f773002b707f09a532a7c8342e875ca
tapaburaco <tapa2019buraco@gmail.com>
2019-04-09 16:51:17 -0300

    2 files modified, 1 deleted

--- app/maps/maps.component.ts
+++ app/maps/maps.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 import { GoogleMaps, GoogleMap, Environment } from '@ionic-native/google-maps';
 
+declare const google;
+
 @Component({
   selector: 'app-maps',
   templateUrl: './maps/maps.component'
 })
 export class MapsComponent{
   
+  map: GoogleMap;
+
+  constructor() {}
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBA8W-66aghPeIqcwdzAluNIfadrB82EHc',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBA8W-66aghPeIqcwdzAluNIfadrB82EHc'
+    });
+
+    this.map = GoogleMaps.create('map_canvas');
+  }
 }
--- pages/home/home.component.ts
+++ pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 //------------testes---------------------------//
 import { ElementRef, ViewChild } from '@angular/core';
@@ -5,54 +6,18 @@ import { IonicPage, NavParams } from 'ionic-angular';
 //---------------------------------------------------------//
 import { DenunciaComponent } from '../denuncia/denuncia.component';
 
-declare const google;
 
-//@IonicPage()
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html'
 })
 export class HomeComponent {
   
-
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
-
-
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams
   ) { }
-/*  
-  map: GoogleMap;
 
-  constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad(){
-    this.loadMap();
-  }
-*/  
-  ionViewDidLoad() {
-    // start my map
-    let posMaceio = { lat: -9.648139, lng: -35.717239 }
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 8,
-      center: posMaceio,
-      mapTypeId: 'roadmap'
-    });
-    this.map.setCenter(posMaceio);
-  }
-/*
-  loadMap(){
-
-    Environment.setEnv({
-      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyBA8W-66aghPeIqcwdzAluNIfadrB82EHc',
-      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyBA8W-66aghPeIqcwdzAluNIfadrB82EHc'
-    });
-
-    this.map = GoogleMaps.create('map_canvas');
-  }
-*/
   onButtonClick(){
     
     this.navCtrl.push(DenunciaComponent);
