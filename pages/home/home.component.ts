20ad65aa55f8bb8c534b5b48681971bb869f403d
tapaburaco <tapa2019buraco@gmail.com>
2019-04-09 00:21:26 -0300

    4 files modified

--- pages/home/home.component.ts
+++ pages/home/home.component.ts
@@ -1,21 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GoogleMaps, GoogleMap, Environment } from '@ionic-native/google-maps';
-
+//------------testes---------------------------//
+import { ElementRef, ViewChild } from '@angular/core';
+import { IonicPage, NavParams } from 'ionic-angular';
+//---------------------------------------------------------//
 import { DenunciaComponent } from '../denuncia/denuncia.component';
 
+declare const google;
+
+@IonicPage()
 @Component({
   selector: 'app-home',
   templateUrl: 'home.html'
 })
 export class HomeComponent {
+  
 
-  map: GoogleMap;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) { }
+
+/*  map: GoogleMap;
 
   constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad(){
+*/
+  /*ionViewDidLoad(){
     this.loadMap();
+  }*/
+  ionViewDidLoad() {
+    // start my map
+    let posMaceio = { lat: -9.648139, lng: -35.717239 }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 8,
+      center: posMaceio,
+      mapTypeId: 'roadmap'
+    });
+    this.map.setCenter(posMaceio);
   }
 
   loadMap(){
