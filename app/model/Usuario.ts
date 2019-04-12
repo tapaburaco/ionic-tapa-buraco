c5f00577bd03837d698da25f916868cbc4906e25
tapaburaco <tapa2019buraco@gmail.com>
2019-04-11 23:52:46 -0300

    2 files modified

--- app/model/Usuario.ts
+++ app/model/Usuario.ts
@@ -1,24 +1,9 @@
-export class Usuario {
+export interface Usuario {
    
+  uid: string;
   nome: string;
   email: string;
   senha: string;
   cpf: number;
-
-  constructor(
-    nome: string,
-    email: string,
-    senha: string,
-    cpf: number
-  ){
-    this.nome = nome;
-    this.email = email;
-    this.senha = senha;
-    this.cpf = cpf;
-  }
-
-  getUsuario(){
-    
-  }
 
 }
--- pages/login/login.component.ts
+++ pages/login/login.component.ts
@@ -2,17 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { FormGroup, FormControl } from '@angular/forms';
 import { NavController } from 'ionic-angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 import { HomeComponent } from '../home/home.component';
+import { Usuario } from '../../app/model/usuario';
 
 @Component({
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
-  constructor(public navCtrl: NavController,
-              public aAuth: AngularFireAuth) 
-  { }
+  constructor(
+    public db: AngularFirestore,
+    public navCtrl: NavController,
+    public aAuth: AngularFireAuth
+  ) 
+  {}
+
+  usuario:  Observable<Usuario>;
 
   public login = new FormGroup({
     email: new FormControl(''),
@@ -25,25 +33,11 @@ export class LoginComponent {
 
     this.aAuth.auth.signInWithEmailAndPassword(email, senha)
       .then((user) => {
-        console.log(user.additionalUserInfo.providerId)
-        console.log(user.additionalUserInfo.isNewUser)
-        console.log(user.additionalUserInfo.profile)
-        console.log(user.additionalUserInfo.username)
-        console.log(user.credential.providerId)
-        console.log(user.credential.signInMethod)
-        console.log(user.operationType)
-        console.log(user.user.displayName)
-        console.log(user.user.email)
-        console.log(user.user.emailVerified)
-        console.log(user.user.isAnonymous)
+        this.usuario = this.db.collection("usuario", ref => 
+        ref.where("UID", "==", user.user.uid)).valueChanges();        
         console.log(user.user.uid)
-        console.log(user.user.refreshToken)
-        console.log(user.user.providerId)
-        console.log(user.user.providerData)
-        console.log(user.user.phoneNumber)
-        console.log(user.user.photoURL)
-        console.log(user.user.metadata)
         //this.navCtrl.push(HomeComponent);
+        //OkQquDnODraq4t9DHC3ArdmJqqk2
       })
       .catch((error) => {
         alert(error.message)
